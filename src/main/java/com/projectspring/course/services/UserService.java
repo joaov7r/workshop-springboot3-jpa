d066610a2d7e808b1d58956a4c6a2a9fd4c5954a
João Vitor <jvc.lopes@outlook.com>
2024-09-18 16:53:45 -0300

    User insert

--- src/main/java/com/projectspring/course/services/UserService.java
+++ src/main/java/com/projectspring/course/services/UserService.java
@@ -2,13 +2,16 @@ package com.projectspring.course.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.projectspring.course.entities.User;
 import com.projectspring.course.repositories.UserRepository;
 
 @Service
 public class UserService {
+
 	@Autowired
 	private UserRepository repository;
 
@@ -19,5 +22,9 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
 }
